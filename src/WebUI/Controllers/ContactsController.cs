5febe2ede124be3735476879468fcd711926ac4b
ken999999999 <keLearning2021@outlook.com>
2022-10-18 17:43:23 +0800

    feature - Contact Controller

---
+++ src/WebUI/Controllers/ContactsController.cs
@@ -0,0 +1,17 @@
+﻿using ContactdocIO.Application.Contacts.Commands.CreateContact;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactdocIO.WebUI.Controllers;
+
+[Authorize]
+public class ContactsController : ApiControllerBase
+{
+    [HttpPost]
+    public async Task<ActionResult<int>> Create(CreateContactCommand command)
+    {
+        return await Mediator.Send(command);
+    }
+
+
+}
