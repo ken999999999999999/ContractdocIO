92b241d84cbd4d6ef80be0707918596cb2efcd77
ken999999999 <keLearning2021@outlook.com>
2022-10-19 16:38:26 +0800

    change login Id to username

--- src/Domain/Entities/IOUser.cs
+++ src/Domain/Entities/IOUser.cs
@@ -7,7 +7,7 @@ public class IOUser
 
     public string Name { get; set; } = default!;
 
-    public string LoginId { get; set; } = default!;
+    public string UserName { get; set; } = default!;
 
     public string Email { get; set; } = default!;
 
--- src/Infrastructure/Persistence/Configurations/IOUserConfiguration.cs
+++ src/Infrastructure/Persistence/Configurations/IOUserConfiguration.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;using Microsoft.EntityFrameworkCore.Metadata.Builders;namespace ContactdocIO.Infrastructure.Persistence.Configurations;public class ApplicationUserConfiguration : IEntityTypeConfiguration<IOUser>{    public void Configure(EntityTypeBuilder<IOUser> builder)    {        builder.Property(t => t.Name).HasMaxLength(200).IsRequired();        builder.Property(t => t.LoginId).HasMaxLength(200).IsRequired();        builder.Property(t => t.Email).IsRequired();        builder.HasIndex(t => t.Email).IsUnique();
+﻿using Microsoft.EntityFrameworkCore;using Microsoft.EntityFrameworkCore.Metadata.Builders;namespace ContactdocIO.Infrastructure.Persistence.Configurations;public class ApplicationUserConfiguration : IEntityTypeConfiguration<IOUser>{    public void Configure(EntityTypeBuilder<IOUser> builder)    {        builder.Property(t => t.Name).HasMaxLength(200).IsRequired();        builder.Property(t => t.UserName).HasMaxLength(200).IsRequired();        builder.Property(t => t.Email).IsRequired();        builder.HasIndex(t => t.Email).IsUnique();
 
-        builder.HasIndex(t => t.LoginId).IsUnique();
+        builder.HasIndex(t => t.UserName).IsUnique();
 
     }
 }
