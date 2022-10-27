17f1981cff44a0f31be21e9cb80043f687a9ce5f
ken999999999 <keLearning2021@outlook.com>
2022-10-27 20:50:49 +0800

    sign in / out redirect action

--- src/WebUI/contractdocio-web/src/auth/AuthConfigProvider.tsx
+++ src/WebUI/contractdocio-web/src/auth/AuthConfigProvider.tsx
@@ -1,4 +1,5 @@
 import { AuthProvider, AuthProviderProps } from 'react-oidc-context';
+import { useNavigate } from 'react-router-dom';
 
 interface IAuthConfigProvider {
   children: JSX.Element;
@@ -7,12 +8,25 @@ interface IAuthConfigProvider {
 const oidConfig: AuthProviderProps = {
   authority: process.env.REACT_APP_AUTH_URL,
   client_id: process.env.REACT_APP_CLIENT_ID,
-  redirect_uri: window.location.origin + '/authentication/login-callback',
+  redirect_uri: `${window.location.origin}/authentication/login-callback`,
   metadataUrl: `${process.env.REACT_APP_AUTH_URL}/.well-known/openid-configuration`,
   scope: 'openid profile ContractdocIO.WebUIAPI',
-  post_logout_redirect_uri: window.location.origin
+  post_logout_redirect_uri: `${window.location.origin}/authentication/logout-callback`
 };
 
 export default ({ children }: IAuthConfigProvider): JSX.Element => {
-  return <AuthProvider {...oidConfig}>{children}</AuthProvider>;
+  const navigate = useNavigate();
+  return (
+    <AuthProvider
+      {...oidConfig}
+      onSigninCallback={(user) => {
+        const state = user?.state as { returnUrl: string };
+        navigate(state?.returnUrl ? state.returnUrl : '/', {
+          replace: true
+        });
+      }}
+    >
+      {children}
+    </AuthProvider>
+  );
 };
