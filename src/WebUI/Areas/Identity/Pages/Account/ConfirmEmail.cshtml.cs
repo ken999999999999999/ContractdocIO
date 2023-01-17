0bca931b8bc43f16f2f622a1e2b9fb3009cfee02
ken999999999 <keLearning2021@outlook.com>
2023-01-16 23:37:49 -0500

    feature - add seed data in confirm email page

--- src/WebUI/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ src/WebUI/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -6,6 +6,8 @@ using System;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ContractdocIO.Application.Common.Interfaces;
+using ContractdocIO.Infrastructure.Persistence.Migrations;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -17,10 +19,12 @@ namespace ContractdocIO.WebUI.Areas.Identity.Pages.Account
     public class ConfirmEmailModel : PageModel
     {
         private readonly UserManager<IOUser> _userManager;
+        private readonly IApplicationDbContext _context;
 
-        public ConfirmEmailModel(UserManager<IOUser> userManager)
+        public ConfirmEmailModel(UserManager<IOUser> userManager, IApplicationDbContext context)
         {
             _userManager = userManager;
+            _context = context;
         }
 
         /// <summary>
@@ -41,6 +45,34 @@ namespace ContractdocIO.WebUI.Areas.Identity.Pages.Account
             {
                 return NotFound($"Unable to load user with ID '{userId}'.");
             }
+            var signedContracts = await _context.SignedContracts.Where(a => a.ReceivedByEmail == user.Email).ToListAsync();
+
+            foreach (var signedContract in signedContracts)
+            {
+                signedContract.ReceivedByUserId = user.Id;
+            }
+
+            var initContract = new Contract();
+            initContract.ContractGroupId = Guid.NewGuid().ToString();
+
+            initContract.Content = "<p>Dear Candidate,</p><p><br></p><p>" +
+                "I am very pleased to offer your the position of IT Specialist. This is a full-time, permanent position with a start date of December 10<sup>th</sup> 2022." +
+                "</p><p><br></p><p>This position will have a starting salary of $60,000.00 per year. " +
+                "Your salary is payable, weekly, less required deduction, " +
+                "</p><p><br></p><p>Your will receive one week of paid vacation per year, pro-rated for your first year if applicable. " +
+                "Vacations are to be taken as such time or times are mutually convenient between the employer and employee." +
+                "</p><p><br></p><p>You will be directly to report to David. " +
+                "</p><p><br></p><p>We look forward to having you join out team and look forward to your response, " +
+                "Should you have any question please don't hesitate to contract me at (111) 111-1111</p><p><br></p><p>Regards, </p><p>HR Team</p><p><br></p>";
+
+            initContract.Type = "Employment";
+            initContract.Title = "Offer of Employment";
+            initContract.Options = new List<Option>() { new Option() { Content = "I accept the offer." } };
+
+            initContract.OwnedByUserId = user.Id;
+            user.Contracts.Add(initContract);
+
+            await _context.SaveChangesAsync(new CancellationToken());
 
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ConfirmEmailAsync(user, code);
